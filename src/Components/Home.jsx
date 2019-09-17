0208b6b0c98edbf023a7e39d8eceb9e835df558b
olinelson <olivernelson@icloud.com>
2019-09-17 19:42:18 -0400

    working on adding mobile sidebar

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -8,7 +8,7 @@ import Advocacy from './Advocacy';
 import Footer from './Footer';
 import IWroteYouASong from './IWroteYouASong';
 
-import { Button, Menu, Header, Sidebar, Icon, Items, Image, Item, Accordion, Divider } from 'semantic-ui-react'
+import { Button, Menu, Header, Segment, Sidebar, Icon, Items, Image, Item, Accordion, Divider } from 'semantic-ui-react'
 import Hieronymus from './Hieronymus';
 import TrashCanDream from './TrashCanDream';
 
@@ -28,6 +28,8 @@ export default function Home(props) {
     const advocacyRef = useRef(null)
     const hieronymusRef = useRef(null)
     const trashCanRef = useRef(null)
+
+    const [sideBarShown, setSideBarShown] = useState(false)
 
     const [appState, setAppState] = useState({
         routesInView: {
@@ -114,7 +116,6 @@ export default function Home(props) {
     return <>
 
 
-
         <Menu pointing style={{ position: 'sticky', top: "0", background: "white", zIndex: 100 }} secondary>
             <SideWaysMenu position="left">
                 <Menu.Item active={props.location.pathname === "/i_wrote_you_a_song"} onClick={() => window.scroll({ top: iWroteYouASongRef.current.offsetTop, behavior: 'smooth' })} content="I Wrote You A Song" />
@@ -131,104 +132,102 @@ export default function Home(props) {
                 <Menu.Item icon="twitter" />
             </Menu.Menu>
         </Menu>
-        <main id="page-wrap">
 
 
 
-            <StyledTrackVisibility {...props} partialVisibility>
-                <News appState={appState} setAppState={setAppState} {...props} />
-            </StyledTrackVisibility>
+        <StyledTrackVisibility {...props} partialVisibility>
+            <News appState={appState} setAppState={setAppState} {...props} />
+        </StyledTrackVisibility>
 
 
-            <RefDivider ref={iWroteYouASongRef} />
-            <StyledTrackVisibility  {...props} partialVisibility>
-                <IWroteYouASong appState={appState} setAppState={setAppState} {...props} />
-            </StyledTrackVisibility>
+        <RefDivider ref={iWroteYouASongRef} />
+        <StyledTrackVisibility  {...props} partialVisibility>
+            <IWroteYouASong appState={appState} setAppState={setAppState} {...props} />
+        </StyledTrackVisibility>
 
 
-            <RefDivider ref={hieronymusRef} />
-            <StyledTrackVisibility partialVisibility {...props} >
-                <Hieronymus {...props} appState={appState} setAppState={setAppState} />
-            </StyledTrackVisibility>
+        <RefDivider ref={hieronymusRef} />
+        <StyledTrackVisibility partialVisibility {...props} >
+            <Hieronymus {...props} appState={appState} setAppState={setAppState} />
+        </StyledTrackVisibility>
 
-            <RefDivider ref={trashCanRef} />
-            <StyledTrackVisibility partialVisibility {...props} >
-                <TrashCanDream {...props} appState={appState} setAppState={setAppState} />
-            </StyledTrackVisibility>
+        <RefDivider ref={trashCanRef} />
+        <StyledTrackVisibility partialVisibility {...props} >
+            <TrashCanDream {...props} appState={appState} setAppState={setAppState} />
+        </StyledTrackVisibility>
 
-            <RefDivider ref={equrlityRef} />
-            <StyledTrackVisibility partialVisibility {...props} >
-                <Equrlity {...props} appState={appState} setAppState={setAppState} />
-            </StyledTrackVisibility>
-
-
-            <RefDivider ref={advocacyRef} />
-            <StyledTrackVisibility partialVisibility {...props} >
-                <Advocacy {...props} appState={appState} setAppState={setAppState} />
-            </StyledTrackVisibility>
+        <RefDivider ref={equrlityRef} />
+        <StyledTrackVisibility partialVisibility {...props} >
+            <Equrlity {...props} appState={appState} setAppState={setAppState} />
+        </StyledTrackVisibility>
 
 
-
-
-            {/* <Footer /> */}
-
-            <div>
-                <Divider hidden />
-                <Divider hidden />
-                <div style={{ background: "rgb(230, 230, 230)" }}>
-
-                    <Container text>
-
-                        <FooterContainer >
-                            <Divider hidden />
-                            <FooterMenu compact secondary>
-                                < Menu.Item
-                                    name='wrote'
-                                    onClick={() => window.scroll({ top: iWroteYouASongRef.current.offsetTop, behavior: 'smooth' })}
-                                    content='I Wrote You A Song'
-                                />
-                                < Menu.Item
-                                    name='hieronymus'
-                                    onClick={() => window.scroll({ top: hieronymusRef.current.offsetTop, behavior: 'smooth' })}
-                                    content='Hieronymus Trio'
-                                />
-                                < Menu.Item
-                                    name='trashCanDream'
-                                    onClick={() => window.scroll({ top: trashCanRef.current.offsetTop, behavior: 'smooth' })}
-                                    content='Trash Can Dream'
-                                />
+        <RefDivider ref={advocacyRef} />
+        <StyledTrackVisibility partialVisibility {...props} >
+            <Advocacy {...props} appState={appState} setAppState={setAppState} />
+        </StyledTrackVisibility>
 
 
 
 
+        {/* <Footer /> */}
 
-                            </FooterMenu>
-                            <FooterMenu compact secondary>
-                                < Menu.Item
-                                    name='equrlity'
-                                    onClick={() => window.scroll({ top: equrlityRef.current.offsetTop, behavior: 'smooth' })}
-                                    content='Equrlity'
-                                />
-                                < Menu.Item
-                                    name='advocacy'
-                                    onClick={() => window.scroll({ top: advocacyRef.current.offsetTop, behavior: 'smooth' })}
-                                    content='Advocacy'
-                                />
+        <div>
+            <Divider hidden />
+            <Divider hidden />
+            <div style={{ background: "rgb(230, 230, 230)" }}>
+
+                <Container text>
+
+                    <FooterContainer >
+                        <Divider hidden />
+                        <FooterMenu compact secondary>
+                            < Menu.Item
+                                name='wrote'
+                                onClick={() => window.scroll({ top: iWroteYouASongRef.current.offsetTop, behavior: 'smooth' })}
+                                content='I Wrote You A Song'
+                            />
+                            < Menu.Item
+                                name='hieronymus'
+                                onClick={() => window.scroll({ top: hieronymusRef.current.offsetTop, behavior: 'smooth' })}
+                                content='Hieronymus Trio'
+                            />
+                            < Menu.Item
+                                name='trashCanDream'
+                                onClick={() => window.scroll({ top: trashCanRef.current.offsetTop, behavior: 'smooth' })}
+                                content='Trash Can Dream'
+                            />
 
 
 
 
-                            </FooterMenu>
 
-                            <Header as='h6' icon='copyright' content={(new Date()).getFullYear() + " Inner Wisdom"} />
+                        </FooterMenu>
+                        <FooterMenu compact secondary>
+                            < Menu.Item
+                                name='equrlity'
+                                onClick={() => window.scroll({ top: equrlityRef.current.offsetTop, behavior: 'smooth' })}
+                                content='Equrlity'
+                            />
+                            < Menu.Item
+                                name='advocacy'
+                                onClick={() => window.scroll({ top: advocacyRef.current.offsetTop, behavior: 'smooth' })}
+                                content='Advocacy'
+                            />
 
-                            <Divider hidden />
-                        </FooterContainer>
 
-                    </Container>
-                </div>
 
+
+                        </FooterMenu>
+
+                        <Header as='h6' icon='copyright' content={(new Date()).getFullYear() + " Inner Wisdom"} />
+
+                        <Divider hidden />
+                    </FooterContainer>
+
+                </Container>
             </div>
-        </main>
+
+        </div>
     </>
 }
