f1093dc6f5b44912d2f17bab517e00e2013202c0
olinelson <olivernelson@icloud.com>
2019-09-18 16:51:43 -0400

    switched to hash router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import 'semantic-ui-css/semantic.min.css'
 import './App.css'
-import { BrowserRouter as Router, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 
 import Home from './Components/Home';
 
